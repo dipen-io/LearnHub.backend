12a78ebb8033f3915c4381602e810a19b6af0034
Dipen Boro <borod9200@gmail.com>
2026-08-14 19:15:09 +0530

    added public banner api/controller

--- src/banners/banners.controller.ts
+++ src/banners/banners.controller.ts
@@ -1,2 +1,22 @@
 // src/banners/banners.controller.ts
 
+import { Controller, Get, Post, Param, Body, Query } from "@nestjs/common";
+import { BannersService } from "./banners.service";
+import { QueryBannersDto } from "./dto/query-banners.dto";
+import { TrackBannerDto } from "./dto/track-banner.dto";
+
+@Controller('banners')
+export class BannersController {
+    constructor(private readonly bannerService: BannersService) { }
+
+    @Get()
+    findActive(@Query() query: QueryBannersDto) {
+        return this.bannerService.findActive(query)
+    }
+
+    @Post(':id/track')
+    track(@Param('id') id: string, @Body() dto: TrackBannerDto) {
+        return this.bannerService.track(id, dto.event);
+    }
+
+}
